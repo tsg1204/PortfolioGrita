b900ecacef4cdaec382ad95b8d7ab94b0c15c020
tsg1204 <tsg1204@gmail.com>
2017-01-22 16:52:08 -0500

    Update sendmail.php

--- sendmail.php
+++ sendmail.php
@@ -2,7 +2,7 @@
 if(isset($_POST['email'])) {
     
     // CHANGE THE TWO LINES BELOW
-    $email_to = "tsg1204@gmail.com";
+    $email_to = "";
     
     $email_subject = "website html form submissions";
     
